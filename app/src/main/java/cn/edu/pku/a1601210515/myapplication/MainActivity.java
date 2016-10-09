5e39ecfc03ad22eacd9ad7bb44c6586eb084592c
qiang fu <1601210515@pku.edu.cn>
2016-10-09 22:37:58 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/a1601210515/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.a1601210515.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by fuqia on 2016/10/9.
+ */
+
+public class MainActivity extends Activity {
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
+
