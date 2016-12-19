2f4f568c67688457a1485ce46c70401bcbe9a626
qiang fu <1601210515@pku.edu.cn>
2016-12-19 09:19:55 +0800

    future weather chage

---
+++ app/src/main/java/cn/edu/pku/fuqiang/bean/FutureWeather.java
@@ -0,0 +1,245 @@
+package pku.zhang.bean;
+
+/**
+ * Created by Zhang on 2015/5/5.
+ */
+public class FutureWeather {
+    private String nextday_week;
+    private String nextday_high;
+    private String nextday_low;
+    private String nextday_climate_day;
+    private String nextday_climate_night;
+    private String nextday_wind;
+
+    private String thirdday_week;
+    private String thirdday_high;
+    private String thirdday_low;
+    private String thirdday_climate_day;
+    private String thirdday_climate_night;
+    private String thirdday_wind;
+
+    private String fourthday_week;
+    private String fourthday_high;
+    private String fourthday_low;
+    private String fourthday_climate_day;
+    private String fourthday_climate_night;
+    private String fourthday_wind;
+
+    private String fifthday_week;
+    private String fifthday_high;
+    private String fifthday_low;
+    private String fifthday_climate_day;
+    private String fifthday_climate_night;
+    private String fifthday_wind;
+
+    @Override
+    public String toString() {
+
+        return "明天：——日期："+ nextday_week
+                +"   最高温度："+ nextday_high
+                + "  最低温度：" +nextday_low
+                + "  白天天气：" + nextday_climate_day
+                + "  晚上天气：" + nextday_climate_night
+                + "  白天风向：" + nextday_wind;
+    }
+
+    public String getNextday_week() {
+        return nextday_week;
+    }
+
+    public void setNextday_week(String nextday_week) {
+        this.nextday_week = nextday_week;
+    }
+
+    public String getNextday_high() {
+        return nextday_high;
+    }
+
+    public void setNextday_high(String nextday_high) {
+        this.nextday_high = nextday_high;
+    }
+
+    public String getNextday_low() {
+        return nextday_low;
+    }
+
+    public void setNextday_low(String nextday_low) {
+        this.nextday_low = nextday_low;
+    }
+
+
+
+    public String getNextday_wind() {
+        return nextday_wind;
+    }
+
+    public void setNextday_wind(String nextday_wind) {
+        this.nextday_wind = nextday_wind;
+    }
+
+    public String getThirdday_week() {
+        return thirdday_week;
+    }
+
+    public void setThirdday_week(String thirdday_week) {
+        this.thirdday_week = thirdday_week;
+    }
+
+    public String getThirdday_high() {
+        return thirdday_high;
+    }
+
+    public void setThirdday_high(String thirdday_high) {
+        this.thirdday_high = thirdday_high;
+    }
+
+    public String getThirdday_low() {
+        return thirdday_low;
+    }
+
+    public void setThirdday_low(String thirdday_low) {
+        this.thirdday_low = thirdday_low;
+    }
+
+
+
+    public String getThirdday_wind() {
+        return thirdday_wind;
+    }
+
+    public void setThirdday_wind(String thirdday_wind) {
+        this.thirdday_wind = thirdday_wind;
+    }
+
+    public String getFourthday_week() {
+        return fourthday_week;
+    }
+
+    public void setFourthday_week(String fourthday_week) {
+        this.fourthday_week = fourthday_week;
+    }
+
+    public String getFourthday_high() {
+        return fourthday_high;
+    }
+
+    public void setFourthday_high(String fourthday_high) {
+        this.fourthday_high = fourthday_high;
+    }
+
+    public String getFourthday_low() {
+        return fourthday_low;
+    }
+
+    public void setFourthday_low(String fourthday_low) {
+        this.fourthday_low = fourthday_low;
+    }
+
+
+
+    public String getFourthday_wind() {
+        return fourthday_wind;
+    }
+
+    public void setFourthday_wind(String fourthday_wind) {
+        this.fourthday_wind = fourthday_wind;
+    }
+
+    public String getFifthday_week() {
+        return fifthday_week;
+    }
+
+    public void setFifthday_week(String fifthday_week) {
+        this.fifthday_week = fifthday_week;
+    }
+
+    public String getFifthday_high() {
+        return fifthday_high;
+    }
+
+    public void setFifthday_high(String fifthday_high) {
+        this.fifthday_high = fifthday_high;
+    }
+
+    public String getFifthday_low() {
+        return fifthday_low;
+    }
+
+    public void setFifthday_low(String fifthday_low) {
+        this.fifthday_low = fifthday_low;
+    }
+
+
+
+    public String getFifthday_wind() {
+        return fifthday_wind;
+    }
+
+    public void setFifthday_wind(String fifthday_wind) {
+        this.fifthday_wind = fifthday_wind;
+    }
+
+    public String getNextday_climate_day() {
+        return nextday_climate_day;
+    }
+
+    public void setNextday_climate_day(String nextday_climate_day) {
+        this.nextday_climate_day = nextday_climate_day;
+    }
+
+    public String getNextday_climate_night() {
+        return nextday_climate_night;
+    }
+
+    public void setNextday_climate_night(String nextday_climate_night) {
+        this.nextday_climate_night = nextday_climate_night;
+    }
+
+    public String getThirdday_climate_day() {
+        return thirdday_climate_day;
+    }
+
+    public void setThirdday_climate_day(String thirdday_climate_day) {
+        this.thirdday_climate_day = thirdday_climate_day;
+    }
+
+    public String getThirdday_climate_night() {
+        return thirdday_climate_night;
+    }
+
+    public void setThirdday_climate_night(String thirdday_climate_night) {
+        this.thirdday_climate_night = thirdday_climate_night;
+    }
+
+    public String getFourthday_climate_day() {
+        return fourthday_climate_day;
+    }
+
+    public void setFourthday_climate_day(String fourthday_climate_day) {
+        this.fourthday_climate_day = fourthday_climate_day;
+    }
+
+    public String getFourthday_climate_night() {
+        return fourthday_climate_night;
+    }
+
+    public void setFourthday_climate_night(String fourthday_climate_night) {
+        this.fourthday_climate_night = fourthday_climate_night;
+    }
+
+    public String getFifthday_climate_day() {
+        return fifthday_climate_day;
+    }
+
+    public void setFifthday_climate_day(String fifthday_climate_day) {
+        this.fifthday_climate_day = fifthday_climate_day;
+    }
+
+    public String getFifthday_climate_night() {
+        return fifthday_climate_night;
+    }
+
+    public void setFifthday_climate_night(String fifthday_climate_night) {
+        this.fifthday_climate_night = fifthday_climate_night;
+    }
+}
